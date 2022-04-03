c1337ec5c79e37a004d5429fe35a0741d3cefc16
Ruben Guedes <imagine@macbook-air-de-ruben.home>
2022-04-03 21:19:52 +0200

    sidebar toggler removed from desktop

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,13 +12,17 @@ const Layout: React.FC<Props> = ({ children }) => {
 
     const [childrenMediaQueryController] = useMediaQuery('(max-width: 700px)')
 
-    const [sideBarOpen, setSideBarOpen] = useState<boolean>(true)
+    const [sideBarOpen, setSideBarOpen] = useState<boolean>(childrenMediaQueryController ? false : true)
 
     return (
         <Box width={"100vw"} overflow="hidden" display={"grid"} position="relative" gridTemplateRows={{ sm: "unset", md: "0.9fr 0.1fr" }} gridAutoColumns={{ sm: "unset", md: "1fr" }} gridGap="0" height={"100vh"} >
-            <Center position="absolute" zIndex="10" top="2rem" marginX="50%" visibility={{ sm: "visible", md: "hidden" }} onClick={() => setSideBarOpen(prev => !prev)} >
-                {sideBarOpen ? <MdClose color="white" fontSize="24px" /> : <MdMenuOpen color="white" fontSize="24px" />}
-            </Center>
+            {
+                childrenMediaQueryController ? (
+                    <Center position="absolute" zIndex="10" top="2rem" marginX="50%" visibility={{ sm: "visible", md: "hidden" }} onClick={() => setSideBarOpen(prev => !prev)} >
+                        {sideBarOpen ? <MdClose color="white" fontSize="24px" /> : <MdMenuOpen color="white" fontSize="24px" />}
+                    </Center>
+                ) : null
+            }
             <Box display="grid" overflowY="hidden" height="full" gridTemplateColumns={{ sm: "1fr", md: "0.2fr 0.8fr" }} gridTemplateRows={{ md: "1fr" }} gridGap="0" >
                 {sideBarOpen ? <Sidebar /> : null}
                 {childrenMediaQueryController ? sideBarOpen ? null : children : children}
